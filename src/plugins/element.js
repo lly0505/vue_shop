cc60839e1160428841e1c4ed1ddd242ab22b84c7
ljb <306033085@qq.com>
2020-04-19 21:13:44 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 导入弹窗提示组件
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+    // 弹窗组件必须挂载在Vue上
+Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        redirect: './login'
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
+//  挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    // to 将要访问的路径
+    // from代表从哪个路径跳转而来
+    // next 是一个函数,表示放行
+    //      next() 放行     next('/login') 强制跳转
+
+    if (to.path === '/login') return next()
+        // 获取token
+    const toKenStr = window.sessionStorage.getItem('token')
+    if (!toKenStr) return next('/login')
+    next()
+})
 export default router
